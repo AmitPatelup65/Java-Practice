d445874975caa8e068c2b1807395a57c7c69d530
AmitPatelup65 <patelboy330@gmail.com>
2025-09-11 23:28:26 +0530

    update

--- Tri.java
+++ Tri.java
@@ -1,10 +1,10 @@
 public class Tri{
    public static void main(String[] args) {
-    int k=1;
+    int m=1;
        for(int i=1;i<=4;i++){
         for(int j=1;j<=i;j++){
-            System.out.print(k+" ");
-             k++; 
+            System.out.print(m+" ");
+             m++; 
         }
         System.out.println();
       
