bfaa1438ffae0860d839ae60a15c3fc9b466d23e
AmitPatelup65 <patelboy330@gmail.com>
2025-09-18 23:52:43 +0530

    update

--- L.java
+++ L.java
@@ -2,7 +2,7 @@ public class L{
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
             for(int j=1;j<=5;j++){
-                if(j==1||i==5||j==5){
+                if(j==1||i==5||j==5 || i==1){
                     System.out.print("* ");
                 }
                 else{
