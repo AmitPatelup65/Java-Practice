c03813f47c6f966e95e7d8efa93e27d6ee869e00
AmitPatelup65 <patelboy330@gmail.com>
2025-09-19 23:40:55 +0530

    update

--- Alpha.java
+++ Alpha.java
@@ -4,9 +4,9 @@ public class Alpha{
         for(int i=1;i<=5;i++){
             for (int j=1;j<=i;j++){
                 System.out.print(ch+" ");
-                ch++;
-            }
-            System.out.println( );
+                ch++; 
+            }          
+            System.out.println();
         }
     }
 }
