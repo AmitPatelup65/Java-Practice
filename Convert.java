4d9cb8eda9ae6bc7e42a65cbf5c9ff4f19f995e9
AmitPatelup65 <patelboy330@gmail.com>
2025-09-23 23:21:30 +0530

    update

--- Convert.java
+++ Convert.java
@@ -9,9 +9,7 @@ public class Convert{
     System.out.println("Enter THe Number : ");
     num=sc.nextInt();
     if(num >=0 && num <=9){
-        // for(int i=0;i<arr.length;i++){
             System.out.println(arr[num]);
-        // }
     }
     else{
         System.out.println("Error");
