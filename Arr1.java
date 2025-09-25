303c7160e337ed1d69675ece8f0e1e046c1ca3aa
AmitPatelup65 <patelboy330@gmail.com>
2025-09-25 23:45:12 +0530

    update

--- Arr1.java
+++ Arr1.java
@@ -5,7 +5,7 @@ public class Arr1{
     }
 }
 class x{
-    int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+    int arr[][]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
     void arr(){
        for(int i=0;i<arr.length;i++){
         for(int j=0;j<3;j++){
