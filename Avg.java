a1825bf375913b4380754575aea6a0e9f4a44a22
AmitPatelup65 <patelboy330@gmail.com>
2025-09-25 23:33:57 +0530

    update

---
+++ Avg.java
@@ -0,0 +1,12 @@
+public class Avg{
+    public static void main(String[] args) {
+       av x=new av();
+       x.average(10,20,30,40,50);
+    }
+}
+class av{
+    
+     void average(int a,int b,int c,int d,int e){
+            System.out.println((a+b+c+d+e)/5);
+        }
+}
