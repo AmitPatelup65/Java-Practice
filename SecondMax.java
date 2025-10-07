5bd8b19e835e0380a314c018c01b755bf652dc87
AmitPatelup65 <patelboy330@gmail.com>
2025-10-07 22:25:53 +0530

    update

--- SecondMax.java
+++ SecondMax.java
@@ -1,25 +1,32 @@
 public  class SecondMax{
     public static void main(String[] args) {
-        int arr[]={9,13,7,1,12};
+        int arr[]={9,13,7,1,12,-4,77,-1};
         int length=arr.length;
         int max=Integer.MIN_VALUE;
+        int min =Integer.MAX_VALUE;
         int secondmax=Integer.MIN_VALUE;
-         int thirdedmax=Integer.MIN_VALUE;
+        int secondmin=Integer.MAX_VALUE;
         for(int i=0;i<length;i++){
             if(arr[i]>max){
                 secondmax=max;
                 max=arr[i];      
             }
+            else if(arr[i]<min){
+                secondmin=min;
+                min=arr[i];
+            }
+            else if (arr[i]>min && arr[i]<secondmin) {
+                secondmin=arr[i];
+            }
           else  if(arr[i]<max && arr[i]>secondmax ){
-               thirdedmax=secondmax;
                 secondmax=arr[i];
             }
-            if ( arr[i]>thirdedmax && arr[i]<secondmax ) {
-                thirdedmax=arr[i];
-            }
+           
         }
         System.out.println("Maximum Value "+ max);
+         System.out.println("Minimum Value "+ min);
         System.out.println("Second Maximum Value "+ secondmax);
-        System.out.println("Second Maximum Value "+ thirdedmax);
+        System.out.println("Second Minimum Value "+ secondmin);
+       
     }
 }
