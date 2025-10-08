5915e873cdfcce58efa1083082bab81ba8b139eb
AmitPatelup65 <patelboy330@gmail.com>
2025-10-08 22:31:07 +0530

    update

--- Max.java
+++ Max.java
@@ -1,6 +1,16 @@
 public class Max{
     public static void main(String[] args) {
-        int []arr={1,2,3,5,4,5,3};
-        
+        int []arr={1,2,5,3,5,4,5,3,5};
+        int max=Integer.MIN_VALUE;
+        int count=1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            else if(max==arr[i]){
+                count++;
+            }
+        }
+        System.out.print(count+" ");
     }
 }
