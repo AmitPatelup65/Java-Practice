63afcf9a7cf254d68942d9b34bdb0f408883ea48
AmitPatelup65 <patelboy330@gmail.com>
2025-10-12 22:29:13 +0530

    update

--- SumOfPair.java
+++ SumOfPair.java
@@ -1,13 +1,13 @@
 public class SumOfPair{
     public static void main (String []args){
         int []arr={7,2,3,6,5};
-        int sum=0;
+        int summ=0;
         int max=0;
         for(int i=0;i<arr.length;i++){
             for (int j=1;j<arr.length;j++){              
-              sum=arr[i]+arr[j];
-              if(sum>max){
-                max=sum;   
+              summ=arr[i]+arr[j];
+              if(summ>max){
+                max=summ;   
               }
                }         
         }
