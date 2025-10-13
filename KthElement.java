10fd9f862b514afc095285b2d6defc15006a042c
AmitPatelup65 <patelboy330@gmail.com>
2025-10-13 23:31:31 +0530

    update

--- KthElement.java
+++ KthElement.java
@@ -5,21 +5,27 @@ public class KthElement{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int input;
-        int []arr={4,1,5,2,3};
+        int index=0;
+        int []arr={6,34,12,19,25};
         for(int i=0;i<arr.length;i++){
+          index++;
             for (int j=0;j<arr.length-1;j++){
               if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
               }
+            
         }
+          
         }
-        System.out.println("Enter The number to get kth maximum Element In The Array : ");
+        System.out.print("Enter The number to get kth maximum Element In The Array : ");
         input=sc.nextInt();
-
-            //  for(int i=0;i<arr.length;i++){
-            //     System.out.print (" "+arr[i]);
-            //  }
+            System.out.println("The Kth Maximum Number is : "+ index);
+            System.out.println("The Kth Maximum Number is : "+ arr[index-input]);
+             System.out.print("Enter The number to get kth maximum Element In The Array : ");
+        input=sc.nextInt();
+            System.out.println("The Kth Minimum Number is : "+ arr[input-1]);
+           
     }
 }
