4243e167cc3383821d1857ef57c6f0b08cafd209
AmitPatelup65 <patelboy330@gmail.com>
2025-10-13 23:33:30 +0530

    update

--- KthElement.java
+++ KthElement.java
@@ -1,31 +1,30 @@
 
 import java.util.Scanner;
 
-public class KthElement{
+public class KthElement {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int input;
-        int index=0;
-        int []arr={6,34,12,19,25};
-        for(int i=0;i<arr.length;i++){
-          index++;
-            for (int j=0;j<arr.length-1;j++){
-              if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-              }
-            
-        }
-          
+        int index = 0;
+        int[] arr = {6, 34, 12, 19, 25};
+        for (int i = 0; i < arr.length; i++) {
+            index++;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+
         }
         System.out.print("Enter The number to get kth maximum Element In The Array : ");
-        input=sc.nextInt();
-            System.out.println("The Kth Maximum Number is : "+ index);
-            System.out.println("The Kth Maximum Number is : "+ arr[index-input]);
-             System.out.print("Enter The number to get kth maximum Element In The Array : ");
-        input=sc.nextInt();
-            System.out.println("The Kth Minimum Number is : "+ arr[input-1]);
-           
+        input = sc.nextInt();
+        System.out.println("The Kth Maximum Number is : " + arr[index - input]);
+        System.out.print("Enter The number to get kth Minimum Element In The Array : ");
+        input = sc.nextInt();
+        System.out.println("The Kth Minimum Number is : " + arr[input - 1]);
+
     }
 }
