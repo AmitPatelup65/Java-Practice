7ccb38b54aca2acd470c364d87c5377070f78e39
AmitPatelup65 <patelboy330@gmail.com>
2025-10-14 23:01:39 +0530

    update

--- Practice1.java
+++ Practice1.java
@@ -4,7 +4,7 @@ public class Practice1{
         int l=arr.length;
         int []res=new int [l];
         for(int i=0;i<arr.length;i++){
-            
+            System.out.print(" "+arr[l-i-1] );
         }
     }
 }
