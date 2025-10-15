1828f5fe905b6d06bc2dd96bc64cc6940f4c3422
AmitPatelup65 <patelboy330@gmail.com>
2025-10-15 20:42:26 +0530

    update

--- Practice1.java
+++ Practice1.java
@@ -1,11 +1,11 @@
 public class Practice1{
     public static void main(String[] args) {
-        // int []arr={1,2,3,4,5};
-        // int l=arr.length;
-        // int []res=new int [l];
-        // for(int i=0;i<arr.length;i++){
-        //     System.out.print(" "+arr[l-i-1] );
-        // }
+        int []arr={1,2,3,4,5};
+        int l=arr.length;
+        int []res=new int [l];
+        for(int i=0;i<arr.length;i++){
+            System.out.print(" "+arr[l-i-1] );
+        }
         System.out.println(10/3);
     }
 }
