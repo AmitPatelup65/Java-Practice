f4c931e2df763e544bc710efac466dbae7ad0636
AmitPatelup65 <patelboy330@gmail.com>
2025-10-16 21:10:20 +0530

    update

---
+++ Frequency.java
@@ -0,0 +1,19 @@
+
+public class Frequency {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 2, 4, 5, 4, 3, 5,};
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " -> "+count);
+
+        }
+
+    }
+}
