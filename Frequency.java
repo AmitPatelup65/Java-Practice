6ab50f20a32c77ed954e3dfcec7056436fb3b986
AmitPatelup65 <patelboy330@gmail.com>
2025-10-16 21:15:44 +0530

    update

--- Frequency.java
+++ Frequency.java
@@ -3,15 +3,19 @@ public class Frequency {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 1, 2, 4, 5, 4, 3, 5,};
-
+          boolean visited[] = new boolean[arr.length];
         for (int i = 0; i < arr.length; i++) {
+            if(visited[i]){
+                continue;
+            }
             int count = 1;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] == arr[j]) {
+                    visited[j]=true;
                     count++;
                 }
             }
-            System.out.println(arr[i] + " -> "+count);
+            System.out.println(arr[i] + " -> "+count+" times");
 
         }
 
