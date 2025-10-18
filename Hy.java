839fbc3d000bf5f6b6345b4b5242d8044e2913da
AmitPatelup65 <patelboy330@gmail.com>
2025-10-18 20:24:12 +0530

    update

--- Hy.java
+++ Hy.java
@@ -1,6 +1,8 @@
 public class Hy{
     public static void main(String[]args){
         int []arr={1,2,3,4};
-        System.out.println(arr);
+       for(int i=0;i<arr.length;i++){
+         System.out.print(arr[i]);
+       }
     }
 }
