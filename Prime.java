788b4c093d4484e973ccb6508f01739ebbd16fd0
AmitPatelup65 <patelboy330@gmail.com>
2025-10-21 20:59:00 +0530

    update

--- Prime.java
+++ Prime.java
@@ -8,8 +8,8 @@ public class Prime{
                 count++;
             }
         }
-        //for(int i=0;i<arr.length;i++){
+      
             System.out.print(count+" -> Primes");
-      //  }
+     
     }
 }
