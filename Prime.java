c58d25d7e6faeea35683568f69cc9513580018ff
AmitPatelup65 <patelboy330@gmail.com>
2025-10-21 14:45:56 +0530

    update

---
+++ Prime.java
@@ -0,0 +1,15 @@
+//[2, 4, 5, 7, 9] → 3 primes
+public class Prime{
+    public static void main(String[] args) {
+        int []arr={2,4,5,7,9};
+        int count=1;
+        for(int i=0;i<arr.length;i++){
+            if((arr[i]%1)==0 && (arr[i]%arr[i])==0){
+                count++;
+            }
+        }
+        //for(int i=0;i<arr.length;i++){
+            System.out.print(count+" -> Primes");
+      //  }
+    }
+}
