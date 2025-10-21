7f061201ee0756154ecec792e1372d9d8f3ca359
AmitPatelup65 <patelboy330@gmail.com>
2025-10-21 14:25:09 +0530

    update

---
+++ Shift.java
@@ -0,0 +1,14 @@
+public class Shift{
+    public static void main(String []args){
+        int []arr={1,2,3,4};
+        int temp=arr[3];
+        
+        for(int i=arr.length-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" "+arr[i]);
+        }
+    }
+}
