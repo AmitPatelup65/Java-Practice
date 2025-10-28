27b88e593310fc0c8eb9ab7d7503b2b6102f4cb4
AmitPatelup65 <patelboy330@gmail.com>
2025-10-28 21:59:45 +0530

    update

--- Charinput.java
+++ Charinput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public  class Charinput{
-    //character input in java 
+    //array character input in java 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Eneter The Size Of An Array");
