1f567baba52489f6f9f89843c39193a4e64453b0
AmitPatelup65 <patelboy330@gmail.com>
2025-11-05 21:23:51 +0530

    update

--- TwoSum.java
+++ TwoSum.java
@@ -2,15 +2,19 @@ public class TwoSum{
     public static void main(String []args){
         int []arr={2,3,4,5};
         int target=9;
+        int check =1;
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length-1;j++){
-                if(arr[i]+arr[j+1]==target){
-                    System.out.println(arr[i]+" "+arr[j+1]);
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(i+" "+j);
+                    check=0;
+                    
                 }
-                else{
-                    System.out.println("Error");
-                }
-            }
+                
+            }   
+            if(check==0){
+                break;
+            }     
         }
     }
 }
