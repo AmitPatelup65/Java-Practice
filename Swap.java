603e47eff673a011d003b282c0a35007f61ae4a2
AmitPatelup65 <patelboy330@gmail.com>
2025-11-06 22:10:21 +0530

    update

--- Swap.java
+++ Swap.java
@@ -1,11 +1,13 @@
-public class Swap{
+
+public class Swap {
+
     public static void main(String[] args) {
-        int a=10;
-    int b=15;
-    System.out.println("Before Swapping "+a+" "+b);
-     a=a+b;
-    b=a-b;
-    a=a-b;
-     System.out.println("After Swapping "+a+" "+b);
+        int a = 10;
+        int b = 15;
+        System.out.println("Before Swapping " + a + " " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After Swapping " + a + " " + b);
     }
 }
