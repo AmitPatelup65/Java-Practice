becc9ed8a982db3ac98e384e28d020a5d8fd56db
AmitPatelup65 <patelboy330@gmail.com>
2025-11-07 20:58:33 +0530

    update

--- Multi.java
+++ Multi.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Multi{
     public static void main(String []args){
        Scanner sc=new Scanner(System.in);
-       System.out.print("Enter The Number");
+       System.out.print("Enter The Number ->");
        int n=sc.nextInt();
        for(int i=1;i<=10;i++){
          System.out.println(n+" * "+i+" = "+n*i);
