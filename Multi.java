22fc805dfc0eacf94188c09d2a1b6160ed10914d
AmitPatelup65 <patelboy330@gmail.com>
2025-11-08 21:47:38 +0530

    update

--- Multi.java
+++ Multi.java
@@ -8,6 +8,7 @@ public class Multi{
        int n=sc.nextInt();
        for(int i=1;i<=10;i++){
          System.out.println(n+" * "+i+" = "+n*i);
+         System.out.println();
        }
     }
 }
---
+++ Palin.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Palin {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eneter The Number");
+        int n = sc.nextInt();
+        int check;
+        int rev = 0;
+        check = n;
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n = n / 10;
+        }
+        if (rev == check) {
+           // return true;
+           System.out.println("Palindrome");
+        } else {
+             System.out.println("Not Palindrome");
+        }
+
+    }
+}
