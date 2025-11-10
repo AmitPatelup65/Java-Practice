e55ffd4c7af4dbbafbfcd5a3a26ff9d859d1817f
AmitPatelup65 <patelboy330@gmail.com>
2025-11-10 19:27:45 +0530

    update

---
+++ Power.java
@@ -0,0 +1,11 @@
+public class Power{
+    public static void main(String[] args) {
+        int a=3;
+        int b=4;
+        int power=1;
+        for (int i = 0; i < b; i++) {
+            power=power*a;
+        }
+        System.out.println("Power Is -> "+ power);
+    }
+}
