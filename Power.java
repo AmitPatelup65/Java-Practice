09de95d79372cbed375f171a5db66182782320ef
AmitPatelup65 <patelboy330@gmail.com>
2025-11-11 21:20:58 +0530

    update

--- Power.java
+++ Power.java
@@ -3,7 +3,7 @@ public class Power{
         int a=3;
         int b=4;
         int power=1;
-        for (int i = 0; i < b; i++) {
+        for (int i = 0; i < b; i++){
             power=power*a;
         }
         System.out.println("Power of a^b is ->"+ power);
---
+++ SumofDigits.java
@@ -0,0 +1,13 @@
+public class SumofDigits{
+    public static void main(String[] args) {
+        int x=123;
+        int sum=0;
+        int a=0;
+       while(x>0){
+        a=x%10;
+        sum=sum+a;
+        x=x/10;
+       }
+       System.out.print(sum);
+    }
+}
