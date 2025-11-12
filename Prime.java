768d7e4f4d2f71da661fc80ab2abb71a8bc4d446
AmitPatelup65 <patelboy330@gmail.com>
2025-11-12 20:54:00 +0530

    update

--- Prime.java
+++ Prime.java
@@ -1,28 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        int[] arr = {2, 4, 5, 7, 9};
-        int count = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            int n = arr[i];
-            boolean isPrime = true; 
-
-            if (n <= 1) {
-                isPrime = false;  
-            } else {
-                for (int j = 2; j <= n / 2; j++) {
-                    if (n % j == 0) { 
-                        isPrime = false;
-                        break; 
-                    }
-                }
-            }
-
-            if (isPrime)
-                count++;
-        }
-
-        System.out.println("Total prime numbers: " + count);
+        
     }
 }
 
