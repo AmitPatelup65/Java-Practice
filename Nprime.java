22a7fccb260c590979e4370811f85decfa617d7c
AmitPatelup65 <patelboy330@gmail.com>
2025-11-14 14:20:46 +0530

    update

---
+++ Nprime.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Nprime{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Eneter The Number");
+        int n=sc.nextInt();
+        for(int i=2;i<=n;i++){
+            boolean check =true;
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0){
+                    check=false;
+                    break;
+                }
+            }
+            if(check==true){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
