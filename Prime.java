246bdf07381562e3c425371aee513f7a60677e64
AmitPatelup65 <patelboy330@gmail.com>
2025-11-14 13:49:58 +0530

    update

--- Prime.java
+++ Prime.java
@@ -1,7 +1,27 @@
 public class Prime {
     public static void main(String[] args) {
-        int a=3;
-        
+        int a=2;
+        boolean check=true;
+        if(a==2){
+             System.out.println("Prime"); 
+             return;
+        }
+        if(a==1){
+            System.out.println("Not Prime");
+            return;
+        }
+       for(int i=2;i<=a/2;i++){
+        if(a%i==0){
+            check=false;
+            break;
+        }
+       }
+       if(check==true){
+        System.out.println("Prime");
+       }
+       else{
+        System.out.println("Not Prime");
+       }
 
 }
 }
