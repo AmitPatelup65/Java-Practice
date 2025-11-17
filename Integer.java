218d51b9b58ce2f9fe3f6da084dc1c4f20a35ade
AmitPatelup65 <patelboy330@gmail.com>
2025-11-17 14:21:52 +0530

    update

---
+++ Integer.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Integer{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter The Number : ");
+        double n=sc.nextDouble();
+        int x=(int)n;
+        if(n-x==0){
+            System.out.println("Integer");
+        }
+        else{
+            System.out.println("Not Integer");
+        }
+    }
+}
