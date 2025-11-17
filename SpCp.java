43c98565022e279feb74bf6f9a7fd295aac94759
AmitPatelup65 <patelboy330@gmail.com>
2025-11-17 15:26:53 +0530

    update

--- SpCp.java
+++ SpCp.java
@@ -9,10 +9,11 @@ public class SpCp{
         System.out.println("Enter The Selling Price : ");
         int  sp=sc.nextInt();  
         if(sp>cp){
-            System.out.println("Profit : "+(sp-cp));
+            int x=sp-cp;
+            System.out.println("Profit : "+(x/(double)cp)*100+"%");
         }
         else  if(sp<cp){
-            System.out.println("Loss : "+(cp-sp));
+            System.out.println("Loss : "+(cp/(double)sp)*100+"%");
         }
           else if(sp==cp){
             System.out.println("No Profit No Loss");
