337077eb7abb635aca642b0e5c5c5b29d7ab5727
AmitPatelup65 <patelboy330@gmail.com>
2025-11-17 14:16:56 +0530

    update

---
+++ Absolute.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Absolute{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter The Number : ");
+        int n=sc.nextInt();
+        if(n<0){
+            n=n*-1;
+        }
+        System.out.println(n);
+    }
+}
