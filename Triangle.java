c8c5c621173f898c5e3160cb4cde8096aa2c6a2c
AmitPatelup65 <patelboy330@gmail.com>
2025-11-18 12:31:04 +0530

    update

---
+++ Triangle.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public  class Triangle{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         System.out.println("Enter The 1st Side Of Triangle");
+        int a=sc.nextInt();
+         System.out.println("Enter The 2nd Side Of Triangle");
+        int b=sc.nextInt();
+         System.out.println("Enter The 3rd Side Of Triangle");
+        int c=sc.nextInt();
+       if(a+b>c && b+c> a && a+c>b){
+        System.out.println("It Is Triangle");
+       }
+       else{
+         System.out.println("It Is Not");
+       }
+    }
+}
