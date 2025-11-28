f7eef207a8c6b92c403990b1f32055522fb7c3d5
AmitPatelup65 <patelboy330@gmail.com>
2025-11-28 21:25:23 +0530

    update

--- Tri.java
+++ Tri.java
@@ -3,7 +3,7 @@ public class Tri{
     int m=1;
        for(int i=1;i<=4;i++){
         for(int j=1;j<=i;j++){
-            System.out.print(m+" " );
+            System.out.print(m+" ");
              m++; 
         }
         System.out.println( );
